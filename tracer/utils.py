cd2af5532c81c114d292e99809394e8a91c611cb
Roman Ivanov <iromcorp@gmail.com>
2018-12-17 18:38:50 +0300

    Add id generator

--- tracer/utils.py
+++ tracer/utils.py
@@ -35,10 +35,76 @@ class ProgressBar:
     if iteration == self.total:
       print()
 
+
+class Id:
+
+  __gen = None
+
+  @staticmethod
+  def next():
+    if Id.__gen is None:
+      Id.__gen = Id._new_gen(0)
+    return next(Id.__gen)
+  
+  @staticmethod
+  def reset(start=0):
+    Id.__gen = Id._new_gen(start)
+  
+  @staticmethod
+  def _new_gen(start):
+    _id = start
+    while True:
+      yield _id
+      _id += 1
+
+
+class ID:
+  __instance = None
+  __gen = None
+  __start = 0
+
+  def __new__(cls):
+    if ID.__instance is None:
+      ID.__instance = super().__new__(cls)
+    return ID.__instance
+  
+  def __init__(self, start=0):
+    if ID.__gen is None:
+      ID.__gen = self._new_gen(start)
+      ID.__start = start
+
+  def __next__(self):
+    return next(ID.__gen)
+  
+  def reset(self, start=None):
+    if start is not None:
+      ID.__start = start
+    ID.__gen = self._new_gen(start)
+
+  def _new_gen(self, start):
+    _id = start
+    while True:
+      yield _id
+      _id += 1
+
 if __name__ == "__main__":
-  from time import sleep
-  total = 100
-  bar = ProgressBar(total)
-  for i in range(total):
-    sleep(4 / total)
-    bar.show(i)
+  # from time import sleep
+  # total = 100
+  # bar = ProgressBar(total)
+  # for i in range(total):
+  #   sleep(4 / total)
+  #   bar.show(i)
+
+  # for _ in range(10):
+  #   print(Id.next())
+  
+  # Id.reset(5)
+
+  # print(Id.next())
+  # print(Id.next())
+
+  for _ in range(10):
+    print(next(ID()))
+  
+  ID().reset(5)
+  print(next(ID()))
