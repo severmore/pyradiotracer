c252944869e615461d4ba69df73dfc88a6e6d006
Roman Ivanov <iromcorp@gmail.com>
2019-02-25 17:46:51 +0300

    Add docstrings to ratracer.tracer module

--- ratracer/tracer.py
+++ ratracer/tracer.py
@@ -1,3 +1,22 @@
+"""
+'ratracer.tracer' contains an engine for ray-tracing based on mirrow reflection 
+method. An example of usage:
+
+  from ratracer import shape, tracer
+
+  scene = {
+    (0,0, 0): (0,0,1),
+    (0,0,10): (0,0,-1),
+    (5,0,0): (-1,0,0),
+    (-5,0,0): (1,0,0),
+  }
+  tr = tracer.Tracer(shape.build(scene))
+  paths, shapes_ids = tr(vec(0,0,5), vec(0,10,5), max_reflections=2)
+
+:class:`ratracer.tracer.Tracer` class implement MR-method. Note, such method is 
+useful to compute scene with specular reflecting shapes.
+"""
+
 import itertools
 import numpy
 
@@ -8,10 +27,28 @@ from ratracer.utils import verbose
 _SHADOWING_INDENT = .99999
 
 class Tracer:
-  """ Image reflection method """
+  """ Implements mirrow reflection method. Contains methods which are as 
+  follows:
+  
+    - :func:`__call__ - run ray-tracing for a scene
+    - :func:`clear` - clear results of previous tracer call; when tracer is 
+        called, cleaning routine run automatically
+    - :func:`get_shapes` - return generator that produces shapes stored in
+        `self.scene` by the sequence of specified ids
+    - :func:`is_shadowed` - check if specified ray intersect any object of
+        scene.
+  """
 
   def __init__(self, scene):
-    """ Performs ray tracing of a given `scene` """
+    """ Performs ray tracing of a given :param:`scene`. Scene itself is a 
+    sequence of shapes located in module :mod:'ratracer.shape'. The tracing 
+    based on mirrow reflection method and it is subject to specular relfections.
+
+    See Also
+    ________
+    .shape.build_shapes
+    .shape.Plane
+    """
     self.scene = {shape.id: shape for shape in scene}
     self._sids = {id_ for id_ in self.scene}
     self.scene[numpy.inf] = shape.Empty()
@@ -19,16 +56,26 @@ class Tracer:
     self._paths = None
     self._shapes = None
   
-  def get_shapes(self, sid_sequence):
-    return (self.scene[sid] for sid in sid_sequence)
-  
-  def clean(self):
-    self._paths = []
-    self._shapes = []
-  
   def __call__(self, tx, rx, max_reflections=2):
-    """ Build rays pathes from `tx` to `rx` with at most `reflection_num` 
-    reflections.
+    """ Build rays pathes from :arg:`tx` to :arg:`rx` with at most 
+    :arg:`max_reflections` reflections.
+
+    Parameters
+    __________
+    tx : `np.ndarray`
+        Starting point for rays
+    rx : `np.ndarray`
+        Ending point for rays
+    max_reflections : int, optional
+        Restict rays with a maximum number of reflection
+    
+    Returns
+    _______
+    paths, sids : list of list of 3-`np.ndarray`, list of turple of int
+        A list of all rays traced and a list of shapes id which caused of
+        reflections of proper rays.
+    
+    
     """
     self.clean()
 
@@ -76,6 +123,15 @@ class Tracer:
       start = ipoint
       path.append(ipoint)
     return path
+
+  def clean(self):
+    """ Clean previously build pathes. """
+    self._paths = []
+    self._shapes = []
+  
+  def get_shapes(self, sid_sequence):
+    """ Get a generator of shapes stored in :param:`self.scene` by its ids. """
+    return (self.scene[sid] for sid in sid_sequence)
   
   def is_shadowed(self, start, end):
     """ Check if ray with `start` and `end` shadowed by any shape of a scene """
@@ -84,7 +140,7 @@ class Tracer:
     return any(shape.is_shadowed(start, end) for shape in self.scene.values())
 
 
-def view(path, *,sep='->'):
+def view(path, *, sep='->'):
   """ Forms string representation of path. """
   if path is None:
     return 'None'
@@ -112,7 +168,6 @@ def _print__traced_path(path, *, color):
   print(f'traced path is {color(view(path))}')
 
   
-
 
 if __name__ == '__main__':
   from utils import vec3d as vec
